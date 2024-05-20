f3569f58267bde52edf8874e3ec640f86b3f71fa
Hacı Celal Aygar <celal.aygar@gmail.com>
2024-05-20 03:14:33 +0300

    Update USBWatcher.java

--- USB_Watcher/USBWatcher.java
+++ USB_Watcher/USBWatcher.java
@@ -4,8 +4,6 @@ import java.nio.file.*;
 public class USBWatcher {
 
     public static void main(String[] args) {
-        // İzlemek istediğiniz dizini belirleyin. Bu örnekte, "E:\\" sürücüsünü izliyoruz.
-        // USB takıldığında mount edilen sürücüyü belirtmelisiniz.
         Path path = Paths.get("E:\\"); 
 
         try (WatchService watchService = FileSystems.getDefault().newWatchService()) {
